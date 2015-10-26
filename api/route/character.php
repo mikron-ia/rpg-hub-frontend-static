448c11b6364936a4f89a5ef3138a6ce376b98712
Mikron <mikronia@gmail.com>
2015-10-26 21:14:22 +0100

    Chief card added

--- api/route/character.php
+++ api/route/character.php
@@ -10,6 +10,7 @@ $app->get('/character/', function (Silex\Application $app) {
         [
             'title' => 'Character list',
             'display' => $app['display'],
+            'list' => true,
         ]
     );
 });
@@ -31,11 +32,29 @@ $app->get('/party/', function (Silex\Application $app) {
  * Display specific character
  */
 $app->get('/character/{id}', function (Silex\Application $app, $id) {
+    $uri = $app['config.deploy']['dataSource'] . '?id=' . $id . '&key=' . $app['config.deploy']['key'];
+
+    $retriever = new \Mikron\HubFront\Domain\Service\Retriever($uri);
+
+    $data = $retriever->getDataAsArray();
+
+    if ($data === null) {
+        throw new \Exception("Character not found", 404);
+    }
+
     return $app['twig']->render(
         'character.twig',
         [
             'title' => 'Character statistics',
             'display' => $app['display'],
+            'list' => false,
+            'characterData' => [
+                'name' => isset($data['name']) ? $data['name'] : "name not given",
+                'attributes' => isset($data['attributes']) ? $data['attributes'] : [],
+                'advantages' => isset($data['advantages']) ? $data['advantages'] : [],
+                'reputations' => isset($data['reputations']) ? $data['reputations'] : [],
+                'contacts' => isset($data['contacts']) ? $data['contacts'] : [],
+            ]
         ]
     );
 });
