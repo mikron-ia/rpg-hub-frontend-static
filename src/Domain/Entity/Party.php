b2da5d745213e528a383051a92f0d8489636e968
Mikron <mikronia@gmail.com>
2015-11-05 15:04:07 +0100

    Working Party class with tests

---
+++ src/Domain/Entity/Party.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Mikron\HubFront\Domain\Entity;
+
+class Party extends BagOfAttributes
+{
+    function createData()
+    {
+        return [
+            'members' => [], // list of party members
+            'reputation' => [], // listing of party reputation
+            'membersReputations' => [], // list of party members' reputation
+        ];
+    }
+}
