5f39cb866dcd51a22b98fc4661f0dd5b3b7a7ab9
Mikron <mikronia@gmail.com>
2015-11-05 15:27:11 +0100

    Data correctly fed to the template

--- api/route/party.php
+++ api/route/party.php
@@ -5,12 +5,24 @@
  * @todo Acquisition of party data from external source
  */
 $app->get('/party/', function (Silex\Application $app) {
+    $uri = $app['config.deploy']['dataSource'] . '?id=party&key=' . $app['config.deploy']['key'];
+
+    $retriever = new \Mikron\HubFront\Domain\Service\Retriever($uri);
+
+    $data = $retriever->getDataAsArray();
+
+    if ($data === null) {
+        throw new \Exception("Party data not found", 404);
+    }
+
+    $party = new \Mikron\HubFront\Domain\Entity\Party($data);
+
     return $app['twig']->render(
         'party.twig',
         [
             'title' => 'Party roster',
             'display' => $app['display'],
-            'partyData' => [],
+            'partyData' => $party->getData(),
         ]
     );
 });
