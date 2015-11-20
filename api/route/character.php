e0f5787627993493eba8919812a167aad40d3597
Mikron <mikronia@gmail.com>
2015-11-20 18:17:45 +0100

    New sub-page for character history

--- api/route/character.php
+++ api/route/character.php
@@ -44,7 +44,7 @@ $app->get('/character/{id}/', function (Silex\Application $app, $id) {
 });
 
 /**
- * Display character with given ID
+ * Display printable sheet for character with given ID
  */
 $app->get('/character/{id}/print/', function (Silex\Application $app, $id) {
     $uri = $app['config']['dataSource'] . '?id=' . $id . '&key=' . $app['config']['key'];
@@ -64,6 +64,34 @@ $app->get('/character/{id}/print/', function (Silex\Application $app, $id) {
         [
             'title' => 'Character printable sheet',
             'display' => $app['display'],
+            'list' => false,
+            'characterData' => $character->getData()
+        ]
+    );
+});
+
+/**
+ * Display history for character with given ID
+ */
+$app->get('/character/{id}/history/', function (Silex\Application $app, $id) {
+    $uri = $app['config']['dataSource'] . '?id=' . $id . '&key=' . $app['config']['key'];
+
+    $retriever = new \Mikron\HubFront\Domain\Service\Retriever($uri);
+
+    $data = $retriever->getDataAsArray();
+
+    if ($data === null) {
+        throw new \Exception("Character not found", 404);
+    }
+
+    $character = new \Mikron\HubFront\Domain\Entity\Character($data);
+
+    return $app['twig']->render(
+        'character_history.twig',
+        [
+            'title' => 'Character history',
+            'display' => $app['display'],
+            'characterKey' => $id,
             'list' => false,
             'characterData' => $character->getData()
         ]
