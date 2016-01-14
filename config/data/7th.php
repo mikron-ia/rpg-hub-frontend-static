9f0e1e07055ab594a4436e3682f7f83a57b92991
Mikron <mikronia@gmail.com>
2016-01-15 00:26:34 +0100

    Configuration data has list of parameters for objects now
    Concerns #59

--- config/data/7th.php
+++ config/data/7th.php
@@ -10,5 +10,59 @@ return [
     'interface' => [
         'titleSuffix' => 'The Tavern',
         'welcome' => 'Welcome to The Tavern!',
-    ]
+    ],
+    'dataPatterns' => [
+        'character' => [
+            'advantages' => [], // traits - advantages & disadvantages
+            'attributes' => [], // basic attributes
+            'basics' => [], // names, descriptions, etc.
+            'catches' => [], // catches: backgrounds, motivations...
+            'contacts' => [], // friends & allies
+            'damage' => [], // more permanent damage
+            'defences' => [], // hit difficulties
+            'descriptions' => [], // descriptions detailed
+            'development' => [], // development history
+            'equipment' => [], // items on person
+            'expenses' => [], // regular expenses
+            'help' => [], // helpful information to display on page
+            'income' => [], // regular income
+            'languages' => [], // languages known
+            'money' => [], // cash at hand
+            'name' => "", // used name
+            'person' => [], //person data
+            'public' => [], // public information
+            'professions' => [], // used together skill groups with skills
+            'rolls' => [], // most commonly used rolls
+            'skillGroups' => [], // thematic skill groups
+            'skills' => [], // skills
+            'stunts' => [], // one-of kind tricks
+            'weapons' => [], // specific catergory of equipment - tools for combat
+            'variables' => [], // often changing values, like counters
+            'xp' => [], // experience
+        ],
+        'person' => [],
+        'group' => [
+            'members' => [], // list of party members
+            'reputations' => [], // listing of party reputation
+            'reputationEvents' => [], // listing of party reputation history
+            'membersReputations' => [], // list of party members' reputation
+            'pastMembers' => [], // retired or dead members
+            'absentMembers' => [], // suspended or currently absent members
+            'help' => [], // helpful information to display on page
+        ],
+        'epic' => [
+            "name" => "", // epic name
+            "current" => [], // current story
+            "stories" => [], // stories description
+            "help" => [], // help boxes
+        ],
+        'event' => [],
+        'story' => [
+            "name" => "", // story name
+            "key" => "", // story key
+            "parameters" => [], // details
+            "short" => "", // recap
+            "long" => "", // full
+        ]
+    ],
 ];
--- config/data/ep.php
+++ config/data/ep.php
@@ -10,5 +10,56 @@ return [
     'interface' => [
         "titleSuffix" => "Data Hub",
         "welcome" => "Welcome to The Data Hub!",
-    ]
+    ],
+    'dataPatterns' => [
+        'character' => [
+            'advantages' => [], // traits - advantages & disadvantages
+            'attributes' => [], // basic attributes
+            'basics' => [], // names, descriptions, etc.
+            'contacts' => [], // friends & allies
+            'damage' => [], // more permanent damage
+            'descriptions' => [], // descriptions detailed
+            'development' => [], // development history
+            'equipment' => [], // items on person
+            'expenses' => [], // regular expenses
+            'help' => [], // helpful information to display on page
+            'income' => [], // regular income
+            'money' => [], // cash at hand
+            'motivations' => [], // character motivations
+            'name' => "", // used name
+            'person' => [], //person data
+            'public' => [], // public information
+            'rolls' => [], // most commonly used rolls
+            'skillGroups' => [], // thematic skill groups
+            'skills' => [], // skills
+            'sleights' => [], // async powers
+            'weapons' => [], // specific category of equipment - tools for combat
+            'variables' => [], // often changing values, like counters
+            'xp' => [], // experience
+        ],
+        'person' => [],
+        'group' => [
+            'members' => [], // list of party members
+            'reputations' => [], // listing of party reputation
+            'reputationEvents' => [], // listing of party reputation history
+            'membersReputations' => [], // list of party members' reputation
+            'pastMembers' => [], // retired or dead members
+            'absentMembers' => [], // suspended or currently absent members
+            'help' => [], // helpful information to display on page
+        ],
+        'epic' => [
+            "name" => "", // epic name
+            "current" => [], // current story
+            "stories" => [], // stories description
+            "help" => [], // help boxes
+        ],
+        'event' => [],
+        'story' => [
+            "name" => "", // story name
+            "key" => "", // story key
+            "parameters" => [], // details
+            "short" => "", // recap
+            "long" => "", // full
+        ]
+    ],
 ];
