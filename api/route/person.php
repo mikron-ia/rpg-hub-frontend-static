633cb24ba5dae6ed2a0d518333ca9165e8874b3e
Mikron <mikronia@gmail.com>
2016-01-23 17:31:29 +0100

    People are listed now
    People list has been created, limited to box display and without pagination

--- api/route/person.php
+++ api/route/person.php
@@ -1,14 +1,54 @@
 <?php
 
+use Mikron\HubFront\Infrastructure\Security\Authentication;
+
 /**
  * List of people
  */
 $app->get('/people/', function (Silex\Application $app) {
+    $authentication = new Authentication(
+        $app['config']['authentication'],
+        'hub',
+        $app['config']['dataSource']['authStrategy']
+    );
+
+    $authMethod = $authentication->provideAuthenticationMethod();
+    $authKey = $authentication->provideAuthenticationKey();
+
+    if ($app['config']['dataSource']['queryUri']) {
+        $uri = $app['config']['dataSource']['uri'] . '?object=people'
+            . '&auth-method=' . $authMethod . '&key=' . $authKey;
+    } else {
+        $uri = $app['config']['dataSource']['uri'] . 'people/'
+            . $authMethod . '/' . $authKey . '/';
+    }
+
+    $retriever = new \Mikron\HubFront\Domain\Service\Retriever($uri);
+
+    $data = $retriever->getDataAsArray();
+
+    if ($data === null) {
+        throw new \Exception("Character not found", 404);
+    }
+
+    $peopleData = [];
+
+    foreach($data['content'] as $person) {
+        $peopleData[] = new \Mikron\HubFront\Domain\Entity\Person($app['config']['dataPatterns'], $person);
+    }
+
+    $peopleArray = [];
+
+    foreach($peopleData as $personData) {
+        $peopleArray[] = $personData->getData();
+    }
+
     return $app['twig']->render(
         'persons.twig',
         [
             'title' => 'People',
             'display' => $app['display'],
+            'peopleData' => $peopleArray
         ]
     );
 });
