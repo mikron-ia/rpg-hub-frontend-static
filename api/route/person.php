fcbcfe1b737b4ab2f84b70ff241a842eae3920cf
Mikron <mikronia@gmail.com>
2016-01-25 23:02:21 +0100

    Person page

--- api/route/person.php
+++ api/route/person.php
@@ -29,13 +29,13 @@ $app->get('/people/', function (Silex\Application $app) {
 
     $peopleData = [];
 
-    foreach($data['content'] as $person) {
+    foreach ($data['content'] as $person) {
         $peopleData[] = new \Mikron\HubFront\Domain\Entity\Person($app['config']['dataPatterns'], $person);
     }
 
     $peopleArray = [];
 
-    foreach($peopleData as $personData) {
+    foreach ($peopleData as $personData) {
         $peopleArray[] = $personData->getData();
     }
 
@@ -48,6 +48,7 @@ $app->get('/people/', function (Silex\Application $app) {
         ]
     );
 });
+
 /**
  * List of all people with a tag
  */
@@ -79,13 +80,13 @@ $app->get('/people/tag/{id}/', function (Silex\Application $app, $id) {
 
     $peopleData = [];
 
-    foreach($data['content'] as $person) {
+    foreach ($data['content'] as $person) {
         $peopleData[] = new \Mikron\HubFront\Domain\Entity\Person($app['config']['dataPatterns'], $person);
     }
 
     $peopleArray = [];
 
-    foreach($peopleData as $personData) {
+    foreach ($peopleData as $personData) {
         $peopleArray[] = $personData->getData();
     }
 
@@ -95,6 +96,48 @@ $app->get('/people/tag/{id}/', function (Silex\Application $app, $id) {
             'title' => 'People',
             'display' => $app['display'],
             'peopleData' => $peopleArray
+        ]
+    );
+});
+
+/**
+ * List complete data of a particular person
+ */
+$app->get('/person/{id}/', function (Silex\Application $app, $id) {
+    $authentication = new Authentication(
+        $app['config']['authentication'],
+        'hub',
+        $app['config']['dataSource']['authStrategy']
+    );
+
+    $accessMethod = 'key';
+    $accessId = $id;
+    $authMethod = $authentication->provideAuthenticationMethod();
+    $authKey = $authentication->provideAuthenticationKey();
+
+    if ($app['config']['dataSource']['queryUri']) {
+        $uri = $app['config']['dataSource']['uri'] . '?object=person'
+            . '&access-method=' . $accessMethod . '&id=' . $accessId
+            . '&auth-method=' . $authMethod . '&key=' . $authKey;
+    } else {
+        $uri = $app['config']['dataSource']['uri'] . 'person/'
+            . $accessMethod . '/' . $accessId . '/'
+            . $authMethod . '/' . $authKey . '/';
+    }
+
+    $retriever = new \Mikron\HubFront\Domain\Service\Retriever($uri);
+
+    $data = $retriever->getDataAsArray();
+
+    $person = new \Mikron\HubFront\Domain\Entity\Person($app['config']['dataPatterns'], $data);
+
+    return $app['twig']->render(
+        'person.twig',
+        [
+            'title' => 'People',
+            'display' => $app['display'],
+            'personData' => $person,
+            'personKey' => $id
         ]
     );
 });
